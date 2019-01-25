67d145f00150444f1ff7e1de89147566dc4ac43f
Natalie Mall <nataliemall@COE-CEE-JH-iMac.local>
2019-01-25 10:53:43 -0800

    Added overall file that runs the overall prisoners dilemma

---
+++ run_prisoners_dilemma.py
@@ -0,0 +1,20 @@
+### Run Nusrat's Game theory code
+
+
+
+from Prisoners_GA import *
+from process_output import *
+from results_plotting import *
+from premade_strategies import *
+from IPD_functions import *
+from GA_functions import *
+
+
+
+
+run_folder = run_prisoners_ga(seed = 0, data_folder = None)
+process_output(run_folder)
+plot_results(run_folder)
+
+
+
