ae62141cbc67d5ce30348bc90728625a18a9ea6a
Nusrat Molla <nmolla13@gmail.com>
2019-01-25 18:36:31 -0800

    change main run script

--- run_prisoners_dilemma.py
+++ run_prisoners_dilemma.py
@@ -1,16 +1,10 @@
-### Run Nusrat's Game theory code
-
-
-
+### Run iterated prisoner's dilemma with default settings 
 from Prisoners_GA import *
 from process_output import *
 from results_plotting import *
 from premade_strategies import *
 from IPD_functions import *
 from GA_functions import *
-
-
-
 
 run_folder = run_prisoners_ga(seed = 0, data_folder = None)
 process_output(run_folder)
